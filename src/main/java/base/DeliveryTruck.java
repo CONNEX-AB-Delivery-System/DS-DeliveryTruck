f002b04d618cba856fd45faf46d0de10b479a0cb
aivars.sablis <aivars.sablis@gmail.com>
2018-03-31 17:57:58 +0200

    first commit

--- src/main/java/base/DeliveryTruck.java
+++ src/main/java/base/DeliveryTruck.java
@@ -1,20 +1,82 @@
 package base;
 
-import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
+//import ev3dev.actuators.lego.motors.EV3LargeRegulatedMotor;
+//import ev3dev.actuators.lego.motors.EV3MediumRegulatedMotor;
 import ev3dev.sensors.Battery;
-import lejos.hardware.port.MotorPort;
+import lejos.robotics.SampleProvider;
+import lejos.hardware.port.SensorPort;
+import ev3dev.sensors.ev3.EV3UltrasonicSensor;
+//import lejos.hardware.sensor.MindsensorsLineLeader;
+//import lejos.hardware.port.MotorPort;
+import ev3dev.sensors.BaseSensor;
+import lejos.hardware.port.Port;
 import lejos.utility.Delay;
+
 
 public class DeliveryTruck {
 
+    //System.out.println("Creating Motor A & B");
+    //motor for steering
+    //final EV3MediumRegulatedMotor motorLeft = new EV3MediumRegulatedMotor(MotorPort.A);
+    //motor for driving
+    //final EV3MediumRegulatedMotor motorRight = new EV3MediumRegulatedMotor(MotorPort.B);
+    //motor for crane rotation
+    //motor for crane lifting
+    //motor for grabber
+
+    //sensor for line reading
+    //sensor for proximity
+    private static EV3UltrasonicSensor sensorProxmity = new EV3UltrasonicSensor(SensorPort.S3);
+    //private static MindsensorsLineLeader lineReader = new MindsensorsLineLeader(SensorPort.S4);
+    private static BaseSensor lineReader = new BaseSensor(SensorPort.S4, "RAW", "ms-line-leader 0x01");
+
+    //Configuration
+    private static int HALF_SECOND = 500;
+
     public static void main(final String[] args){
 
-        System.out.println("Creating Motor A & B");
-        final EV3LargeRegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.A);
-        final EV3LargeRegulatedMotor motorRight = new EV3LargeRegulatedMotor(MotorPort.B);
+        System.out.println("Votage: " + Battery.getInstance().getVoltage());
+        SampleProvider sp;
+
+        int distanceValue = 0;
+
+        String name;
+        //lineReader.wakeUp();
+        name = lineReader.getName();
+        System.out.println("Sensor name: " + name);
+
+        for(int i = 0; i <= 20; i++) {
+
+            sp = sensorProxmity.getListenMode();
+            int sampleSize = sp.sampleSize();
+            float[] sample = new float[sampleSize];
+            sp.fetchSample(sample, 0);
+
+            Delay.msDelay(1000);
+
+            sp = sensorProxmity.getDistanceMode();
+            sampleSize = sp.sampleSize();
+            sample = new float[sampleSize];
+            sp.fetchSample(sample, 0);
+
+            distanceValue = (int)sample[0];
+
+            System.out.println("Iteration: {}, Distance: {}" + i + " "+ distanceValue);
+
+            //sp2 = lineReader.getRedMode();
+
+
+            //System.out.println("" + sp2);
+
+        }
+
+        //lineReader.sleep();
+
+        //System.out.println(Battery.getInstance().getVoltage());
+
 
         //To Stop the motor in case of pkill java for example
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+       /* Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             public void run() {
                 System.out.println("Emergency Stop");
                 motorLeft.stop();
@@ -27,13 +89,26 @@ public class DeliveryTruck {
         motorRight.brake();
 
         System.out.println("Defining motor speed");
-        final int motorSpeed = 200;
+        final int rotateTo = 10;
+        final int motorSpeed = 300;
+        //motorLeft.rotateTo(rotateTo);
         motorLeft.setSpeed(motorSpeed);
         motorRight.setSpeed(motorSpeed);
 
+        motorLeft.getSpeed()
+
+        if (motorLeft.getSpeed() > 200) then {motorLeft.stop()} else {}
+
+
+        {
+            motorLeft.forward();
+
+        }
+        while (sensorX.getValue() > 200)
+
         System.out.println("Go Forward with the motors");
         motorLeft.forward();
-        motorRight.forward();
+        //motorRight.backward();
 
         Delay.msDelay(2000);
 
@@ -43,13 +118,13 @@ public class DeliveryTruck {
 
         System.out.println("Go Backward with the motors");
         motorLeft.backward();
-        motorRight.backward();
+        //motorRight.forward();
 
         Delay.msDelay(2000);
 
         System.out.println("Stop motors");
         motorLeft.stop();
-        motorRight.stop();
+        motorRight.stop(); */
 
         System.out.println("Checking Battery");
         System.out.println("Votage: " + Battery.getInstance().getVoltage());
